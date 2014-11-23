bad16ae91da358f3aa4f3e6a348fd938d80c0b55
bobozeta <bobo_zeta@motel-on-jupiter.sakuraweb.com>
2014-11-24 01:49:29 +0900

    Add fly animation type

--- animation/AnimationMap.h
+++ animation/AnimationMap.h
@@ -14,6 +14,7 @@ class AnimationMap {
   enum AnimationType {
     kAnimationIdle,
     kAnimationWalk,
+    kAnimationFly,
     kAnimationDamaged,
     kAnimationChant,
   /* max number is (2^16)-1 */
