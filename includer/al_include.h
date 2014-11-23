1182200bebafa76ca78fc4bb6fdd568a9b9ee5fc
bobozeta <bobo_zeta@motel-on-jupiter.sakuraweb.com>
2014-11-23 14:44:26 +0900

    Add module to include ALURE headers

---
+++ includer/al_include.h
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) 2014 The Motel on Jupiter
+ */
+#ifndef MOJGAMELIB_INCLUDER_ALINCLUDE_H_
+#define MOJGAMELIB_INCLUDER_ALINCLUDE_H_
+
+#ifdef MOJGAMELIB_WITH_ALURE
+
+#include <AL/al.h>
+#include <AL/alc.h>
+#include <AL/alure.h>
+
+#endif /* MOJGAMELIB_WITH_ALURE */
+
+#endif /* MOJGAMELIB_INCLUDER_ALINCLUDE_H_ */
