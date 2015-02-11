3c8c6291b84a73a23f2c1cdac967bd1c388d303d
bobozeta <bobo_zeta@motel-on-jupiter.sakuraweb.com>
2015-02-11 18:34:42 +0900

    Define BgmPlayer

---
+++ audio/BgmPlayer.cc
@@ -0,0 +1,83 @@
+/**
+ * Copyright (C) 2015 The Motel on Jupiter
+ */
+#include "mojgame/audio/BgmPlayer.h"
+#include "mojgame/includer/al_include.h"
+#include "mojgame/logging/Logger.h"
+
+#ifdef MOJGAMELIB_WITH_ALURE
+
+namespace mojgame {
+
+AlureBgm::AlureBgm(const char *bgm_file_path) :
+  bgm_file_path_(bgm_file_path),
+  source_(0),
+  stream_(nullptr) {
+}
+
+bool AlureBgm::Initialize(float pitch, float gain) {
+  alGenSources(1, &source_);
+  if (alGetError() != AL_NO_ERROR) {
+    mojgame::LOGGER().Error("Failed to create OpenAL source");
+    alureShutdownDevice();
+    return false;
+  }
+  alSourcef(source_, AL_PITCH, pitch);
+  alSourcef(source_, AL_GAIN, gain);
+
+  alureStreamSizeIsMicroSec(AL_TRUE);
+  stream_ = alureCreateStreamFromFile(bgm_file_path_.c_str(), 250000, 0, nullptr);
+  if(stream_ == nullptr) {
+    mojgame::LOGGER().Error("Failed to create stream with bgm file "
+                            "(bgm: %s, errmsg: %s)",
+                            bgm_file_path_.c_str(), alureGetErrorString());
+    alDeleteSources(1, &source_);
+    return 1;
+  }
+  return true;
+}
+
+void AlureBgm::ChangePitch(float pitch) {
+  alSourcef(source_, AL_PITCH, pitch);
+}
+
+void AlureBgm::ChangeGain(float gain) {
+  alSourcef(source_, AL_GAIN, gain);
+}
+
+bool AlureBgm::Finalize() {
+  ALboolean ok = alureDestroyStream(stream_, 0, nullptr);
+  if (!ok) {
+    mojgame::LOGGER().Warn("Failed to destory stream (errmsg: %s)",
+                           alureGetErrorString());
+  }
+  alDeleteSources(1, &source_);
+  return ok == AL_TRUE;
+}
+
+static void OnPlayingFinish(void *userdata, ALuint source) {
+  UNUSED(userdata);
+  UNUSED(source);
+}
+
+bool AlureBgmPlayer::Play(AlureBgm &bgm, int loopcount) {
+  if(!alurePlaySourceStream(bgm.source(), bgm.stream(), 3, loopcount, OnPlayingFinish, nullptr)) {
+    mojgame::LOGGER().Error("Failed to play bgm (errmsg: %s)",
+                            alureGetErrorString());
+    return false;
+  }
+  return true;
+}
+
+bool AlureBgmPlayer::Stop(AlureBgm &bgm) {
+  if(!alureStopSource(bgm.source(), AL_FALSE)) {
+    mojgame::LOGGER().Error("Failed to stop bgm (errmsg: %s)",
+                            alureGetErrorString());
+    return false;
+  }
+  return true;
+}
+
+} /* namespace mojgame */
+
+#endif /* MOJGAMELIB_WITH_ALURE */
---
+++ audio/BgmPlayer.h
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2015 The Motel on Jupiter
+ */
+#ifndef MOJGAMELIB_AUDIO_BGMPLAYER_H_
+#define MOJGAMELIB_AUDIO_BGMPLAYER_H_
+
+#include "mojgame/includer/al_include.h"
+
+#ifdef MOJGAMELIB_WITH_ALURE
+
+namespace mojgame {
+
+class AlureBgm {
+ public:
+  AlureBgm(const char *bgm_file_path);
+  ~AlureBgm() {
+  }
+
+  bool Initialize(float pitch, float gain);
+  bool Finalize();
+
+  void ChangePitch(float pitch);
+  void ChangeGain(float gain);
+
+  const std::string &bgm_file_path() const {
+    return bgm_file_path_;
+  }
+  ALuint source() const {
+    return source_;
+  }
+  alureStream *stream() const {
+    return stream_;
+  }
+
+ private:
+  std::string bgm_file_path_;
+  ALuint source_;
+  alureStream *stream_;
+};
+
+namespace AlureBgmPlayer {
+  bool Play(AlureBgm &bgm, int loopcount);
+  bool Stop(AlureBgm &bgm);
+};
+
+#endif /* MOJGAMELIB_WITH_ALURE */
+
+} /* namespace mojgame */
+
+#endif /* MOJGAMELIB_AUDIO_BGMPLAYER_H_ */
